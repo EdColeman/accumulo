0d97708882d0603dea8255013ba4bf53c55af765
Ed Coleman etcoleman <edcoleman@apache.org>
2021-06-30 14:10:00 +0000

    checkpoint

---
+++ server/base/src/main/java/org/apache/accumulo/server/util/PropUtil.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.server.util;
+
+import org.apache.accumulo.core.conf.Property;
+import org.apache.accumulo.server.ServerContext;
+import org.apache.accumulo.server.conf2.PropCacheId;
+import org.apache.accumulo.server.conf2.PropStoreException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PropUtil {
+
+  private static final Logger log = LoggerFactory.getLogger(PropUtil.class);
+
+  /**
+   * Utility class with static methods - prevent direct construction.
+   */
+  private PropUtil() {}
+
+  public static boolean setNamespaceProperty(final ServerContext context,
+      final PropCacheId propCacheId, final String property, final String value)
+      throws PropStoreException {
+    return false;
+  }
+
+  public static boolean isPropertyValid(String property, String value) {
+    Property p = Property.getPropertyByKey(property);
+
+    log.info("Prop: {}", p);
+
+    if (p == null) {
+      log.info("true isNull");
+    }
+
+    return (p == null || p.getType().isValidFormat(value))
+        && Property.isValidTablePropertyKey(property);
+  }
+}
