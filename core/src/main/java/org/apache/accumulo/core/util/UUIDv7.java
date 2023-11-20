815861e588672498487a4365d78acfe60301c668
Ed <dev1@etcoleman.com>
2023-11-19 21:25:20 -0500

    checkpoint

--- core/src/main/java/org/apache/accumulo/core/util/UUIDv7.java
+++ core/src/main/java/org/apache/accumulo/core/util/UUIDv7.java
@@ -21,7 +21,10 @@ package org.apache.accumulo.core.util;
 import java.security.SecureRandom;
 import java.time.Instant;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Generate a time based UUID based on version 7.
@@ -31,33 +34,118 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class UUIDv7 {
 
-  private static final int ver = 0x07000000; // 0x07;
-  private static final byte var = 0x02;
+  private static final Logger LOG = LoggerFactory.getLogger(UUIDv7.class);
+  private static final int field_ver = 0x7000; // 0x07;
+  private static final byte field_var = 0x02;
 
   private static final int MAX_COUNT = 4096;
-  private volatile Instant timestamp = null;
-  private AtomicInteger counter = new AtomicInteger();
-  private byte[] rand = new byte[8];
-  public UUIDv7(){
-    SecureRandom random = new SecureRandom();
-    random.nextBytes(rand);
+
+  private static final int NANO_BITS = 4096; // 12 bits.
+  private static final double SCALE_NANO = NANO_BITS / 1_000_000_000.0; // max nano + 1.
+  private Instant timestamp;
+  private int counter = 0;
+
+  private long overflowCounter = 0;
+  private int highCount = 0;
+
+  private final SecureRandom random = new SecureRandom();
+  private final byte[] rand = new byte[8];
+
+  public UUIDv7() {
     timestamp = Instant.now();
-    timestamp.getEpochSecond();
+    random.nextBytes(rand);
   }
 
-  public UUID next() {
-    byte[] buffer = new byte[16];
+  public synchronized UUID next() {
 
-    int offset = 0;
-    long time = timestamp.getEpochSecond();
-    return null;
+    Instant now = Instant.now();
+
+    if (now.toEpochMilli() > timestamp.toEpochMilli()) {
+      counter = 0;
+      timestamp = now;
+      return generate();
+    }
+
+    if (now.toEpochMilli() == timestamp.toEpochMilli()) {
+      counter++;
+      highCount = Math.max(counter, highCount);
+      if (counter >= MAX_COUNT) {
+        counter = 0;
+        overflowCounter++;
+        // LOG.info("counter overflow, pausing for next system tick");
+        timestamp = timestamp.plusMillis(1);
+      }
+    }
+    return generate();
+  }
+
+  public long getOverflowCounter() {
+    return overflowCounter;
+  }
+
+  public int getHighCount() {
+    return highCount;
+  }
+
+  UUID generate() {
+    long msb = timestamp.toEpochMilli() << 16;
+    msb |= field_ver;
+    msb |= counter;
+
+    // LOG.info(String.format("%08x, %04x", timestamp.toEpochMilli(), counter));
+    // LOG.info("nano: {}", timestamp.getNano());
+    return new UUID(msb, 0);
+  }
+
+  /**
+   * Scale the {@link Instant#getNano()} value to 12 bits. The max possible value for nanos is
+   * 999_999_999, so this scaling is equivalent to (value / 1,000,000,000.0) * 4096, truncated to an
+   * integer. The resolution of this scaling is 409 uSec.
+   * <p>
+   * For example:
+   *
+   * <pre>
+   *     value = 999,999,999 (max) = (999,999,999 / 1,000,000,000.0) * 4096 = 4095.
+   *     value = 409,600 (min) = (409,600 . 1,000,000,000.0) * 4096 = 1;
+   * </pre>
+   *
+   * @param value nano portion of an Instant
+   * @return value scaled to 12 bits resolution
+   */
+  int nanoScale12Bits(final int value) {
+    return (int) (value * SCALE_NANO);
   }
 
   /**
    *
    * @return UUID formatted as hex string separated by hyphens in standard 8-4-4-4-12 format.
    */
-  public String toString(){
+  public String toString() {
     return "a";
+  }
+
+  public interface NanoClock {
+    long nanoTime();
+  }
+
+  public class SystemNano implements NanoClock {
+    @Override
+    public long nanoTime() {
+      return System.nanoTime();
+    }
+  }
+
+  public class TestNano implements NanoClock {
+    private final AtomicLong time;
+
+    public TestNano(final AtomicLong source) {
+      time = source;
+    }
+
+    @Override
+    public long nanoTime() {
+      return time.get();
+    }
+
   }
 }
--- core/src/test/java/org/apache/accumulo/core/util/UUIDv7Test.java
+++ core/src/test/java/org/apache/accumulo/core/util/UUIDv7Test.java
@@ -18,21 +18,70 @@
  */
 package org.apache.accumulo.core.util;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
 class UUIDv7Test {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UUIDv7Test.class);
-    @Test
-    public void byteOrderTest(){
-        LOG.info("native:{}", ByteOrder.nativeOrder());
-        ByteBuffer origBuff = ByteBuffer.allocate(100);
-        LOG.info("buffer: {}", origBuff.order());
+  private static final Logger LOG = LoggerFactory.getLogger(UUIDv7Test.class);
+
+  @Test
+  public void byteOrderTest() {
+    LOG.info("native:{}", ByteOrder.nativeOrder());
+    ByteBuffer origBuff = ByteBuffer.allocate(100);
+    LOG.info("buffer: {}", origBuff.order());
+  }
+
+  @Test
+  public void shifts() {
+    LOG.info("all: {}", 0x7fffffff);
+    LOG.info("all: {}", String.format("%08x", 999_999_999));
+    LOG.info("all: {}", 0xffffffff >>> 20);
+    LOG.info("all: {}", 999_999_999 >>> 20);
+
+    LOG.info("all: {}", scale12(999_999_999));
+    LOG.info("all: {}", scale12(409_600));
+
+    LOG.info("12_2: {}", scale12_2(999_999_999));
+    LOG.info("12_2: {}", scale12_2(409_600));
+
+    LOG.info("mills: {}", TimeUnit.NANOSECONDS.toMicros(409_600));
+
+    LOG.info("Scale: {}", SCALE_NANO * TimeUnit.SECONDS.toNanos(1));
+
+    LOG.info("shift: {}", (1 << 2));
+
+  }
+
+  int scale12(final int v) {
+    int x = (int) ((v / 1_000_000_000.0) * 4096);
+    return x;
+  }
+
+  private static final int NANO_BITS = 4096; // 12 bits.
+  private static final double SCALE_NANO = NANO_BITS / 1_000_000_000.0; // max nano + 1.
+
+  int scale12_2(final int v) {
+    int x = (int) (v * SCALE_NANO);
+    return x;
+  }
+
+  @Test
+  public void g1() {
+    UUIDv7 gen = new UUIDv7();
+    for (int i = 0; i < 100_000; i++) {
+      UUID u = gen.next();
+      if (i % 5000 == 0) {
+        LOG.info("U{}: {}", i, u);
+      }
     }
+    LOG.info("overflow: {}", gen.getOverflowCounter());
+    LOG.info("highest: {}", gen.getHighCount());
+  }
 }
